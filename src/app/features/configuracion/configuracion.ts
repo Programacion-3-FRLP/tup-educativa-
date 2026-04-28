934e004fe3bc74177bb3a0642fc44504fcb26d18
Ignacio Echave <nacho9847@gmail.com>
2026-04-28 16:48:50 -0300

    feat: pantalla configuracion con logout funcional

--- src/app/features/configuracion/configuracion.ts
+++ src/app/features/configuracion/configuracion.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-configuracion',
+  standalone : true,
   imports: [],
   templateUrl: './configuracion.html',
   styleUrl: './configuracion.css',
 })
 export class Configuracion {
+private router = inject(Router);
 
+logout() {
+  const confirmacion = confirm('¿Seguro que querés cerrar sesión?');
+
+  if (confirmacion) {
+    sessionStorage.removeItem('auth');
+    this.router.navigate(['/login']);
+  }
+}
 }
