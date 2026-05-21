f34e1fef7eca74438eba9a41b4c7920a864519ef
ampi-cs50 <152340325+ampi-cs50@users.noreply.github.com>
2026-05-21 20:14:22 -0300

    fix main

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,26 +1,8 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  importProvidersFrom,
-} from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-providers: [
-  provideRouter(routes),
-  provideClientHydration(withEventReplay()),
-  provideHttpClient(withFetch()),
-
-  importProvidersFrom(
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
-      defaultLanguage: 'es',
-    })
-  ),
-]
+  providers: [provideRouter(routes)],
+};
