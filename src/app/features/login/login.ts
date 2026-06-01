3ec0ff284a1770764eea5617907dacc582450dca
ampi-cs50 <152340325+ampi-cs50@users.noreply.github.com>
2026-06-01 16:11:59 -0300

    Remove innecessary await and use of the new path

--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { AuthService } from '../../core/auth.service';
+import { AuthService } from '@core/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -15,7 +15,7 @@ export class Login {
 
   constructor(
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
   ) {}
 
   async login() {
@@ -25,7 +25,7 @@ export class Login {
       const result = await this.authService.loginWithGoogle();
       console.log('Usuario logueado:', result.user);
 
-      await this.router.navigate(['/items']);
+      this.router.navigate(['/items']);
     } catch (error) {
       console.error('Error al iniciar sesión con Google', error);
       this.loading = false;
